fa31e61f6c99c4bfa94817eddf19c65cc68d8ea7
Nedelcho Dinev <ned.dinev@gmail.com>
2021-11-16 12:13:16 +0200

    Add files via upload

---
+++ Chrismas-tree.js
@@ -0,0 +1,13 @@
+function chrismasTree(n) {
+  n = Number(n);
+  console.log(" ".repeat(n + 1) + "|");
+  for (let row = 0; row < n; row++) {
+    console.log(
+      `${" ".repeat(n - 1 - row)}${"*".repeat(row + 1)} | ${"*".repeat(
+        row + 1
+      )} `
+    );
+  }
+}
+
+chrismasTree(4);
